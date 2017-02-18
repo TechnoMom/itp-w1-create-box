9b6af39ff6a53f58c7d3de474bf4429fb5ca4199
Cyn Armistead <cyn@cynthiaarmistead.com>
2017-02-18 19:55:32 +0000

    Second attempt at group project

--- create_box/main.py
+++ create_box/main.py
@@ -1,10 +1,8 @@
 """This is the entry point of the program."""
 
 def create_box(height, width, character):
-    for i in range(height):
-        box = width*character + '\n'
-        box *= height
-    return box
+    my_box = width*character + '\n'
+    return height * my_box
 
 if __name__ == '__main__':
     my_box = create_box(3, 4, '*')
