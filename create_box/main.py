5c3955874bc8e15639e733825ac4710417db33fc
Cyn Armistead <cyn@cynthiaarmistead.com>
2017-02-19 19:14:29 +0000

    solution

--- create_box/main.py
+++ create_box/main.py
@@ -1,10 +1,17 @@
 """This is the entry point of the program."""
 
 def create_box(height, width, character):
-    line = character*width + '\n'
-    box = line*height
-    return box
+	box = ''
+	line = width * character + '\n'
+	counter = 0
+	while counter < height:
+		box += line
+		counter += 1
+	return box
     
 if __name__ == '__main__':
     box = create_box(3, 4, '*')
     print(box)
+
+
+# :D :D :D haha!
