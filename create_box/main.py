68954a7e9ed369f326c3d293a6b192bdfb6f4d9b
Cyn Armistead <cyn@cynthiaarmistead.com>
2017-02-19 03:03:11 +0000

    third attempt at create box

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,14 @@
 """This is the entry point of the program."""
 
-def create_box(height, width, character):
-    my_box = width*character + '\n'
-    return height * my_box
+def create_box(height, width, char):
+	count = 0
+	box = ''
+	while count < height:
+		box += char * width + '\n'
+		count += 1
+	print(box)
+	return box
 
 if __name__ == '__main__':
-    my_box = create_box(3, 4, '*')
-    print(my_box)
+    box = create_box(3, 4, '*')
+    print(box)
